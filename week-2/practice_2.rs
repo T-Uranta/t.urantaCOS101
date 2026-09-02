1a87205e51873a3983a4edbb8e86a42b4c962369
T-Uranta <tamunoinyene.uranta@pau.edu.ng>
2026-09-02 22:52:10 +0100

    commit

---
+++ week-2/practice_2.rs
@@ -0,0 +1,10 @@
+fn main() {
+
+	println!(); // prints just a 
+
+	println!("hello ");//prints
+
+	println!("format {} arguments", "some"); //prints format some arguments
+
+	println!("Myname is {}. I am the class rep of stream 1", "Dagogo William-Jumbo")
+}
